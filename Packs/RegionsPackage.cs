6aaeb7f5f4d2ea0698b3791fa5976418fb1c15da
Muhammad <muhammadarch22@gmail.com>
2024-04-25 23:49:57 +0500

    Bug fixning

---
+++ Packs/RegionsPackage.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_5.Packages
+{
+    public class RegionsPackage
+    {
+        /// See the available regions at the link https://github.com/bchavez/Bogus/
+        public void AddRegion(string localeCode, string countryName)
+        {
+            if (Regions.ContainsKey(countryName) || Regions.ContainsValue(localeCode))
+            {
+                return;
+            }
+            Regions.Add(countryName, localeCode);
+        }
+        public bool CheckRegion(string regionName)
+        {
+            bool result = Regions.ContainsKey(regionName);
+
+            return result;
+        }
+
+        // Add available regions
+        public Dictionary<string, string> Regions { get; private set; } = new Dictionary<string, string>();
+    }
+}
